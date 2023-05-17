8040a7d3def5dae2c7aab1061ef65f306834c3d5
KebronDaniel <kebronjrdaniel@gmail.com>
2023-05-17 11:18:29 +0300

    Initial commit (Chuck norris Encrypts)

---
+++ Chuck Norris Cipher Encoder (Java)/task/src/chucknorris/Main.java
@@ -0,0 +1,55 @@
+package chucknorris;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        inputToBinary();
+    }
+
+    private static void inputToBinary() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input string: ");
+        String userInput = scanner.nextLine();
+        System.out.println("The result:");
+        String bitRepresentation = "";
+        String firstBlockZero="00";
+        String firstBlockOne="0";
+        for (int i = 0; i < userInput.length(); i++) {
+            char x = userInput.charAt(i);
+            int binary = Integer.parseInt(Integer.toBinaryString(x));
+//            System.out.printf("%c = %07d\n",x,Integer.parseInt(Integer.toBinaryString(x)));
+            bitRepresentation = bitRepresentation + binary;
+        }
+        System.out.println(bitRepresentation);
+        String chuckNorrisCode = "";
+        for (int i = 0; i < bitRepresentation.length(); i++) {
+            int count = 1;
+            while (i + 1 < bitRepresentation.length() &&
+                    bitRepresentation.charAt(i) == bitRepresentation.charAt(i + 1)){
+                i++;
+                count++;
+            }
+            System.out.println(bitRepresentation.charAt(i) + "Has count of " + count);
+
+            if (bitRepresentation.charAt(i) == '1'){
+                chuckNorrisCode = chuckNorrisCode + firstBlockOne + " ";
+            }
+            else {
+                chuckNorrisCode = chuckNorrisCode + firstBlockZero + " ";
+            }
+            for (int j = 0; j < count; j++) {
+                chuckNorrisCode = chuckNorrisCode + "0";
+            }
+            chuckNorrisCode = chuckNorrisCode + " ";
+            System.out.println(chuckNorrisCode);
+
+        }
+        System.out.println("Chuck Norris Code : " + chuckNorrisCode);
+    }
+
+
+
+}
